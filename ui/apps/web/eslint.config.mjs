14429da96092fe0f3c5c46b2f8585b47872cdafb
adem <ceperadem@outlook.com>
2026-01-15 18:31:29 +0300

    add ui

---
+++ ui/apps/web/eslint.config.mjs
@@ -0,0 +1,14 @@
+import { nextJsConfig } from "@workspace/eslint-config/next-js";
+
+/** @type {import("eslint").Linter.Config[]} */
+export default [
+  ...nextJsConfig,
+  {
+    ignores: [
+      ".next/**",
+      "out/**",
+      "build/**",
+      "next-env.d.ts",
+    ],
+  },
+];
